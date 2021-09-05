02f749e3e22086093765cc2ecc2deec0ad8327ef
exersalza <squidodle44@gmail.com>
2021-09-05 12:25:28 +0200

    implement class

--- client.py
+++ client.py
@@ -1,9 +1,7 @@
-import time
 import bcrypt
 
 from server.client_conn import send, client
 from server.sql.sql_conn import mydb
-
 
 # todo:
 #   login if exit then fuckle oyu
@@ -14,59 +12,62 @@ cursor = mydb.cursor(buffered=True)
 
 f = True
 
-## LOGIN PREPARE ## how does the computer scientist duck? NAT NAT NAT :)
 mod = input('What do you want to do?: login/register ').lower()
 
-
-def form():
-    global usr_name, psw
-    usr_name = input("Username: ")
-    psw = input('Password: ')
+## LOGIN PREPARE ## how does the computer scientist duck? NAT NAT NAT :)
 
 
-def register(username, password):
-    global psw, usr_name, f
-    # print(cursor.execute(f"SELECT * FROM login_db WHERE username='{username}'"))
+class Client:
+    def __init__(self):
+        pass
 
-    if cursor.execute(f"SELECT * FROM main_db WHERE username='{username}'"):
-        print('cocks')
-    else:
-        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-        hashed_password = str(hashed_password).strip("b '")
-        cursor.execute(f"INSERT INTO main_db (username, password) VALUES (?, '{hashed_password}')", username)
-        f = False
-
-        send('register_redirect')
-
-        msg = client.recv(1024)
-        print(msg.decode("utf-8"))
-
-        mydb.commit()
-
-        return send('!l')
-
-
-def login(username, password):
-    global usr_name, psw, f
-    cursor.execute(f"SELECT * FROM main_db WHERE username='{username}'")
-    data = cursor.fetchone()
-    check_psw = str.encode(password)
-    if bcrypt.checkpw(check_psw, data[2].encode('utf-8')):
-        send('login_redirect')
-
-        msg = client.recv(1024)
-        print(msg.decode("utf-8"))
-
-        f = False
-    else:
-        print('Password or Username are incorrect!')
+    def form(self) -> None:
+        global usr_name, psw
         usr_name = input("Username: ")
         psw = input('Password: ')
 
-    send('!l')
+    def register(username, password):
+        global psw, usr_name, f
+        # print(cursor.execute(f"SELECT * FROM login_db WHERE username='{username}'"))
 
+        if cursor.execute(f"SELECT * FROM main_db WHERE username='{username}'"):
+            print('cocks')
+        else:
+            hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+            hashed_password = str(hashed_password).strip("b '")
+            cursor.execute(f"INSERT INTO main_db (username, password) VALUES (?, '{hashed_password}')", username)
+            f = False
+
+            send('register_redirect')
+
+            msg = client.recv(1024)
+            print(msg.decode("utf-8"))
+
+            mydb.commit()
+
+            return send('!l')
+
+    def login(username, password):
+        global usr_name, psw, f
+        cursor.execute(f"SELECT * FROM main_db WHERE username='{username}'")
+        data = cursor.fetchone()
+        check_psw = str.encode(password)
+        if bcrypt.checkpw(check_psw, data[2].encode('utf-8')):
+            send('login_redirect')
+
+            msg = client.recv(1024)
+            print(msg.decode("utf-8"))
+
+            f = False
+        else:
+            print('Password or Username are incorrect!')
+            usr_name = input("Username: ")
+            psw = input('Password: ')
+
+        send('!l')
 
 ## LOGIN / REGISTER ##
+
 
 try:
     while f:
@@ -80,7 +81,7 @@ try:
                 psw = input('Password: ')
 
                 if not psw == 'exit':
-                    login(usr_name, psw)
+                    Client.login(usr_name, psw)
 
         elif mod == 'register':
             usr_name = input("Username: ")
@@ -92,7 +93,7 @@ try:
                 psw = input('Password: ')
 
                 if not psw == 'exit':
-                    register(usr_name, psw)
+                    Client().register(usr_name, psw)
 
         else:
             if mod == 'exit':
